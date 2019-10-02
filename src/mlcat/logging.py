e92716d512345d970655930057b84dd2beaf67f8
Mathias Sundholm <mathias_sundholm@hotmail.com>
2019-10-02 09:29:29 +0200

    history now a list of dicts instead of dict of list

--- src/mlcat/logging.py
+++ src/mlcat/logging.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import csv
+import json
 
 class MetricWatcher():
     def __init__(self, watch_vars):
         self.watch_vars = watch_vars
-        self.history = {x : [] for x in watch_vars}
-        self.metrics = {}
+        self.history = [] # list of historic metrics
+        self.last_metric = {}
 
     def __enter__(self):
         return self
@@ -14,17 +16,20 @@ class MetricWatcher():
 
     def update(self, scope):
         
+        metric = {}
         for var in self.watch_vars:
-            self.metrics[var] = scope.get(var, "")
-            self.history[var].append(self.metrics[var])
-        title = ""
+            metric[var] = scope.get(var, "")
+            
+        self.history.append(metric)
+        self.last_metric = metric
+        
         
     def print_metrics(self, metrics=None):
         if metrics is None:
             metrics = self.watch_vars
         
         metrics_str = "".join(
-            ["{}: {} | ".format(k,v) for k, v in self.metrics.items() if k in metrics]
+            ["{}: {} | ".format(k,v) for k, v in self.last_metric.items() if k in metrics]
         )
         print("| {} ".format(metrics_str))
     
@@ -33,10 +38,24 @@ class MetricWatcher():
         if metrics is None:
             metrics = self.watch_vars
         for var in metrics:
-            plt.plot(self.history[var],label=var)
+            data = [x[var] for x in self.history]
+            plt.plot(data,label=var)
         if show:
             plt.legend()
             plt.show()
+
+    # def export_csv():
+
+    #     for 
+
+    #     csv_dict = {"test": 1, "testing": 2}
+
+    #     with open('mycsvfile.csv', 'wb') as f:  # Just use 'w' mode in 3.x
+    #         w = csv.DictWriter(f, my_dict.keys())
+    #         w.writeheader()
+    #         w.writerow(my_dict)
+
+    # def export_json():
 
 ## Tests
 
